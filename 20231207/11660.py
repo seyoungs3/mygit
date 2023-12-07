24f580bdefd4fc7e1a77f4fd0941c99359418c84
Seyoung <7019992@naver.com>
2023-12-07 23:33:43 +0900

    second commit

--- 20231207/11660.py
+++ 20231207/11660.py
@@ -8,10 +8,10 @@ for i in range(n):
     line = list(map(int, input().split()))
     arr.append(line)
 
-presum = [[0 for j in range(len(line))] for _ in range(n)]
+presum = [[0 for _ in range(n+1)] for _ in range(n+1)]
 for i in range(1,n+1):
-    for j in range(1,len(line)+1):
-        presum[i][j] = presum[i][j-1]+presum[i-1][j]-presum[i-1][j-1]+arr[i][j]
+    for j in range(1,n+1):
+        presum[i][j] = presum[i][j-1]+presum[i-1][j]-presum[i-1][j-1]+arr[i-1][j-1]
 
 for i in range(m):
     x1,y1,x2,y2 = map(int, input().split())
