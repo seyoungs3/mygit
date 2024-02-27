c6d5df76394d33bc897b70d6831db45fa242f5ae
Seyoung <7019992@naver.com>
2024-02-27 16:21:09 +0900

    sort1

--- 20240227/11728.py
+++ 20240227/11728.py
@@ -1,34 +1,18 @@
 # 배열 합치기
 
-# 시간초과
+# merge sort로 안했음
+
+import sys
+
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
-sorted = []
-while True:
-    if not a and not b:
-        break
-    if not a:
-        sorted.append(b[0])
-        del b[0]
-        continue
-    if not b:
-        sorted.append(a[0])
-        del a[0]
-        continue
-    elif a[0] < b[0]:
-        sorted.append(a[0])
-        del a[0]
-    elif a[0] > b[0]:
-        sorted.append(b[0])
-        del b[0]
-    elif a[0] == b[0]:
-        sorted.append(a[0])
-        del a[0]
-        del b[0]
-    
-for s in sorted:
-    print(s, end=' ')
+c = a + b
+
+c = sorted(c)
+
+print(*c)
